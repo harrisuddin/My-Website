c9d4946792c91bcc16c5c1b5abd96f2accd3173b
harrisuddin <harris7001@gmail.com>
2019-06-16 03:15:24 +0100

    wrote script to automatically adjust image size depending on window width

--- scripts.js
+++ scripts.js
@@ -1,25 +1,51 @@
     var width = $(window).width();
     addContainerClass(width);
+    updateBCBF(width);
+    updateCodeImage(width);
+    $("#test").text(width);
     $(window).on('resize', function() {
         if ($(this).width() != width) {
             width = $(this).width();
             console.log(width);
+//            $("#test").text(width); testing
         }
+//        location.reload()
         addContainerClass(width);
+        updateBCBF(width);
+        updateCodeImage(width);
     });
 
     function addContainerClass(width) {
-        if (width > 1024) {
+        if (width >= 1300) {
             $("main").addClass("container");
         } else {
             $("main").removeClass("container");
         }
     }
 
-    function updateBCBF() {
-        var num = $("#aboutme-cont").outerHeight(true);
-        $("#bcbf img").height(num);
-        $("#bcbf img").width(num);
+    function updateBCBF(widthNum) {
+        if (widthNum >= 1005) { //&& widthNum <= 1024
+            var aboutmeWidth = $("#aboutme-cont").outerHeight(true);
+
+            if ($(window).width() < aboutmeWidth + $("#aboutme-cont").outerWidth(true)) {
+                var diff = (aboutmeWidth + $("#aboutme-cont").outerWidth(true)) - $(window).width();
+                aboutmeWidth = aboutmeWidth - diff;
+            }
+
+//            $("#aboutme-cont").addClass("width-60");
+//            $("#bcbf").addClass("width-40");
+//            $("#bcbf img").css("margin-top", "0");
+            $("#bcbf img").height(aboutmeWidth);
+            $("#bcbf img").width(aboutmeWidth);
+//            $("#test").text(width + "Yep"); // testing
+
+        }
+    }
+
+    function updateCodeImage(widthNum) {
+        var codeWidth = $("#code-image").outerWidth(true);
+        var codeHeight = 4000 / (6000/codeWidth);
+        $("#code-image").height(codeHeight);
     }
 
     var count = 1;
